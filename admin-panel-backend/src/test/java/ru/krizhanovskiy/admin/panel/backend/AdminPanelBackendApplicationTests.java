ef1bed1225787c6f980db98faaf2c3e6362cec4a
Krizhanovskiy <mrdoka1@mail.ru>
2026-03-22 16:41:19 +0300

    init

---
+++ admin-panel-backend/src/test/java/ru/krizhanovskiy/admin/panel/backend/AdminPanelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.krizhanovskiy.admin.panel.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
